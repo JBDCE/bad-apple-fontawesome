69213eac7e210182496d4c8e7d4e4b629c8cabfa
JBDCE <61785226+JBDCE@users.noreply.github.com>
2026-01-10 22:55:15 +0100

    Add dummy method for merging frames back together

--- video_processor.py
+++ video_processor.py
@@ -1,13 +1,13 @@
 from subprocess import run
 
-def split_videofile(filepath):
+def split_videofile(input_filepath):
     # Split of the audio file from the video to be able to reattach it later
     # TODO I need to figure out how to memorize the metadata.
     # Probably just reuse the source file maybe?
     run([
         'ffmpeg',
         '-i',
-        filepath,
+        input_filepath,
         '-vn',
         '-acodec',
         'copy',
@@ -17,9 +17,14 @@ def split_videofile(filepath):
     run([
         'ffmpeg',
         '-i',
-        filepath,
+        input_filepath,
         'tmp/frames/frame%04d.png',
     ])
 
+def merge_frames():
+    # TODO here another ffmpeg run is used to merge
+    # the split and processed picture files back into an output moviefile
+    pass
+
 if __name__ == '__main__':
-    split_videofile(filepath='Bad-Apple.mp4')
+    split_videofile(input_filepath='Bad-Apple.mp4')
